a222c8082b908fc6db273fd6b80961cc2fe16fe2
kinixkt <kinixh@gmail.com>
2024-10-29 17:29:39 +0900

    メモアプリ　DEMO  ver2.0.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import{ loadMemos, saveMemos } from "./loadMemos";
-import ToDoList from "./ToDoList";
+import ToDoList from "./todolist";
 
 export default function Home() {
   const [memos, setMemos] = useState([
